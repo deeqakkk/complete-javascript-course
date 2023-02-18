1ab28292f8b9755a0ded2eda833dd74651b9e375
Deepak <0822deepakverma@gmail.com>
2023-02-18 12:00:13 +0530

    modal completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const closeBtn = document.querySelector('.close-modal');
+const openBtns = document.querySelectorAll('.show-modal');
+const overlayDiv = document.querySelector('.overlay');
+const modalDiv = document.querySelector('.modal');
+
+
+const closeModal = function() {
+    overlayDiv.classList.toggle('hidden');
+    modalDiv.classList.toggle('hidden');
+};
+for (let i = 0; i < openBtns.length; i++) {
+    openBtns[i].addEventListener('click', closeModal);
+};
+
+closeBtn.addEventListener('click', closeModal);
+overlayDiv.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+})
